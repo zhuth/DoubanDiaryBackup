f226f3ac158e3aedcda573b616a135ece58fe18c
ZTH <zthpublic@gmail.com>
2013-11-18 14:28:26 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DoubanDiaryBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.web = new System.Windows.Forms.WebBrowser();
+            this.notes = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.开始抓取ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrDownload = new System.Windows.Forms.Timer(this.components);
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(645, 405);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(645, 430);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.web);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.notes);
+            this.splitContainer1.Size = new System.Drawing.Size(645, 405);
+            this.splitContainer1.SplitterDistance = 413;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // web
+            // 
+            this.web.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.web.Location = new System.Drawing.Point(0, 0);
+            this.web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web.Name = "web";
+            this.web.Size = new System.Drawing.Size(413, 405);
+            this.web.TabIndex = 1;
+            this.web.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.web_DocumentCompleted);
+            // 
+            // notes
+            // 
+            this.notes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.notes.Location = new System.Drawing.Point(0, 0);
+            this.notes.MinimumSize = new System.Drawing.Size(20, 20);
+            this.notes.Name = "notes";
+            this.notes.Size = new System.Drawing.Size(228, 405);
+            this.notes.TabIndex = 1;
+            this.notes.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.notes_DocumentCompleted);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始抓取ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(645, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 开始抓取ToolStripMenuItem
+            // 
+            this.开始抓取ToolStripMenuItem.Name = "开始抓取ToolStripMenuItem";
+            this.开始抓取ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.开始抓取ToolStripMenuItem.Text = "开始抓取";
+            this.开始抓取ToolStripMenuItem.Click += new System.EventHandler(this.开始抓取ToolStripMenuItem_Click);
+            // 
+            // tmrDownload
+            // 
+            this.tmrDownload.Interval = 10;
+            this.tmrDownload.Tick += new System.EventHandler(this.tmrDownload_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 430);
+            this.Controls.Add(this.toolStripContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "抓取豆瓣日记";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 开始抓取ToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser web;
+        private System.Windows.Forms.WebBrowser notes;
+        private System.Windows.Forms.Timer tmrDownload;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DoubanDiaryBackup
+{
+    public partial class Form1 : Form
+    {
+        int step = -1, start = 0;
+        string peopleId = "";
+        Queue<string> urls = new Queue<string>();
+        Dictionary<string, bool> downloaded = new Dictionary<string, bool>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            web.Navigate("http://www.douban.com/");
+        }
+
+        private void 开始抓取ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            web.Navigate("http://www.douban.com/mine/notes");
+            step = 0;
+        }
+
+        private void error(string message)
+        {
+            MessageBox.Show(message);
+            step = -1; peopleId = "";
+            web.Navigate("http://www.douban.com/");
+        }
+
+        private void web_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+        }
+
+        private void web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            switch (step)
+            {
+                case 0: // 登录
+                    for (int i = 0; i < e.Url.Segments.Length; ++i)
+                        if (e.Url.Segments[i].ToLower() == "people/" && i < e.Url.Segments.Length - 1)
+                        {
+                            peopleId = e.Url.Segments[i + 1]; peopleId = peopleId.Substring(0, peopleId.Length - 1);
+                        }
+                    if (peopleId == "")
+                    {
+                        error("导航失败，请登录后重试。");
+                    }
+                    else
+                    {
+                        this.Text = "已登录 - " + peopleId;
+                        step = 1; start = 0;
+                        urls.Clear();
+                        web.Navigate(string.Format("http://www.douban.com/people/{0}/notes?start={1}", peopleId, start));
+                        return;
+                    }
+                    break;
+
+                case 1: // 抓取
+                    int c = 0;
+                    foreach (HtmlElement link in web.Document.Links)
+                    {
+                        string ls = link.GetAttribute("href");
+                        if (ls.ToLower().StartsWith("http://www.douban.com/note/") && ls.EndsWith("/"))
+                        {
+                            ++c;
+                            if (!downloaded.ContainsKey(ls))
+                            {
+                                urls.Enqueue(ls); downloaded.Add(ls, true);
+                            }
+                        }
+                    }
+                    if (c > 0)
+                        tmrDownload.Enabled = true;
+                    else
+                    {
+                        error("下载队列完毕！");
+                    }
+                    break;
+            }
+        }
+
+        private void notes_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!e.Url.AbsolutePath.Contains("note/")) return;
+            string id = e.Url.Segments[e.Url.Segments.Length - 1];
+            if (id.EndsWith("/")) id = id.Substring(0, id.Length - 1);
+
+            if (!Directory.Exists(peopleId)) Directory.CreateDirectory(peopleId);
+            foreach (HtmlElement div in notes.Document.GetElementsByTagName("div"))
+            {
+                if (div.GetAttribute("className") == "article")
+                {
+                    File.WriteAllText(peopleId + "\\" + id + ".html", div.InnerHtml);
+                    break;
+                }
+            }
+            tmrDownload.Enabled = true;
+        }
+
+        private void tmrDownload_Tick(object sender, EventArgs e)
+        {
+            if (urls.Count > 0)
+            {
+                string s = urls.Dequeue();
+                notes.Navigate(s);
+            }
+            else
+            {
+                start += 10;
+                web.Navigate(string.Format("http://www.douban.com/people/{0}/notes?start={1}", peopleId, start));
+            }
+            tmrDownload.Enabled = false;
+        }
+    }
+}
